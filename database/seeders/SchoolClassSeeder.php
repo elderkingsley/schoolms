73fa64a3bd98827b11520330b8a963ebfeb1c098
Kingsley Akanihu <elderkingsley@gmail.com>
2026-03-24 17:08:53 +0100

    reseed

--- database/seeders/SchoolClassSeeder.php
+++ database/seeders/SchoolClassSeeder.php
@@ -10,14 +10,18 @@ class SchoolClassSeeder extends Seeder
     public function run(): void
     {
         $classes = [
-            ['name' => 'Nursery 1',   'level' => 'Nursery',  'order' => 1],
-            ['name' => 'Nursery 2',   'level' => 'Nursery',  'order' => 2],
-            ['name' => 'Primary 1',   'level' => 'Primary',  'order' => 3],
-            ['name' => 'Primary 2',   'level' => 'Primary',  'order' => 4],
-            ['name' => 'Primary 3',   'level' => 'Primary',  'order' => 5],
-            ['name' => 'Primary 4',   'level' => 'Primary',  'order' => 6],
-            ['name' => 'Primary 5',   'level' => 'Primary',  'order' => 7],
-            ['name' => 'Primary 6',   'level' => 'Primary',  'order' => 8],
+
+            ['name' => 'Creche & Daycare',   'level' => 'Creche',  'order' => 1],
+            ['name' => 'Playgroup',   'level' => 'Playgroup',  'order' => 2],
+            ['name' => 'Pre-Nursery',   'level' => 'Pre-Nursery',  'order' => 3],
+            ['name' => 'Nursery 1',   'level' => 'Nursery',  'order' => 4],
+            ['name' => 'Nursery 2',   'level' => 'Nursery',  'order' => 5],
+            ['name' => 'Primary 1',   'level' => 'Primary',  'order' => 6],
+            ['name' => 'Primary 2',   'level' => 'Primary',  'order' => 7],
+            ['name' => 'Primary 3',   'level' => 'Primary',  'order' => 8],
+            ['name' => 'Primary 4',   'level' => 'Primary',  'order' => 9],
+            ['name' => 'Primary 5',   'level' => 'Primary',  'order' => 10],
+            ['name' => 'Primary 6',   'level' => 'Primary',  'order' => 11],
         ];
 
         foreach ($classes as $class) {
